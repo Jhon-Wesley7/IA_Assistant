bdb0b2fbcd444e7401573aaf0c7a9c643dbfdd39
Jhon Wesley <jwbf2016@gmail.com>
2024-06-18 14:39:38 -0300

    att da IA
    Foram corrigidos alguns bugs, e implementado alguns novos comandos.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('.tallk');
+const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 
 function speak(text) {
@@ -28,3 +28,49 @@ window.addEventListener('load', () => {
     speak("Initializing LEXI...");
     wishMe();
 });
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript.toLowerCase());
+};
+
+btn.addEventListener('click', () => {
+    content.textContent = "Listening...";
+    recognition.start();
+});
+
+function takeCommand(message) {
+    if(message.includes('hey') || message.includes('hello')) {
+        speak("Hello Sir, How May I Help You?");
+    } else if (message.includes("open google")) {
+        window.open("https://www.google.com", "_blank");
+        speak("Abrindo o Google");
+    } else if (message.includes("open youtube")) {
+        window.open("https://www.youtube.com", "_blank");
+        speak("Abrindo o Youtube");
+    } else if (message.includes("open github")) {
+        window.open("https://www.github.com/Jhon-Wesley7/", "_blank");
+        speak("Abrindo o seu Github");
+    }else if (message.includes("open chat")) {
+        window.open("https://www.chatgpt.com/", "_blank");
+        speak("Abrindo um dos meus irmãos");
+    } else if (message.includes("calculadora")) {
+        window.open("https://www.google.com/search?q=calculadora");
+        speak("Abrindo a Calculadora");
+    } else if (message.includes("open WhatsApp")) {
+        window.open("https://www.web.whatsapp.com", "_blank");
+        speak("Abrindo o seu WhatsApp");
+    } else if (message.includes("open animes")) {
+        window.open("https://www.crunchyroll.com/pt-br/", "_blank");
+        speak("Abrindo o melhor site de animes");
+    } else {
+        window.open(`https://www.google.com/search?q=${message.replace(" ", "+")}`, "_blank");
+        const finalText = "I found some information for " + message + " on Google";
+        speak(finalText);
+    }
+}
